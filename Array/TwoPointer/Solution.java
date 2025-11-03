d51fcb0f5cf4a0938ce00c1e043f153a74b2e1a0
Pranta Saha <prantasaha907@gmail.com>
2025-11-03 11:21:41 +0600

    Two Sum Apply Two Pointer

--- Array/TwoPointer/Solution.java
+++ Array/TwoPointer/Solution.java
@@ -3,7 +3,9 @@ package Array.TwoPointer;
 import java.util.Arrays;
 
 class Solution {
-    public int[] twoSum(int[] nums,int target){
+    // Brute-force approach  complexity = n^2
+
+    /* public int[] twoSum(int[] nums,int target){
         int n = nums.length;
         for(int i = 0;i < n-1;i++){
             for(int j = i+1;j < n;j++){
@@ -12,6 +14,37 @@ class Solution {
                 }
             }
         }
+        return new int[]{}; */
+
+        //Two Pointer Approach
+
+    public int[] twoSum(int[] nums,int target){
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+        int left = 0; int right = sorted.length - 1;
+
+        while (left < right) {
+            int sum = sorted[left] + sorted[right];
+            if (sum == target) {
+                int i = -1; int j = -1;
+                for(int k = 0;k < nums.length;k++){
+                    if (nums[k] == sorted[left] && i == -1) {
+                        i = k;
+                    }
+                    else if (nums[k] == sorted[right] && j == -1) {
+                        j = k;
+                    }
+                }
+                return new int[]{i,j};
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+
         return new int[]{};
     }
     public static void main(String[] args) {
