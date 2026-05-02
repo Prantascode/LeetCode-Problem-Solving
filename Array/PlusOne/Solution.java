8450eef84355e29fc907d46b62c8d1d10a32ec72
Pranta Saha <prantasaha907@gmail.com>
2026-05-02 21:43:21 +0600

    Plus One

---
+++ Array/PlusOne/Solution.java
@@ -0,0 +1,29 @@
+package Array.PlusOne;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n-1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+
+                return digits;
+            }
+
+            digits[i] = 0;
+
+        }
+        digits =  new int[n+1];
+
+        digits[0] = 1;
+        return digits;
+    }
+    public static void main(String[] args) {
+         int[] digits = {1,3,4,9};
+         System.out.println(Arrays.toString(plusOne(digits)));
+
+    }
+}
