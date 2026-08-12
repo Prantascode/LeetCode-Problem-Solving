4aea0d937fd719f40ae21728e19037fc3f1390ae
Pranta Saha <prantasaha907@gmail.com>
2026-08-12 21:01:46 +0600

    Time: 24 ms (53.46%) | Memory: 65.5 MB (42.70%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -8,11 +8,8 @@ class Solution {
         map.put(0, -1);
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                prefix -= 1;
-            }else{
-                prefix += 1;
-            }
+            prefix += (nums[i] == 1) ? 1 : -1;
+
 
             if (map.containsKey(prefix)) {
                 
